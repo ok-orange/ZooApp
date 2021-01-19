b4a473184e64d1e3434fd87576107a40e93bcb86
ok-orange <66663494+ok-orange@users.noreply.github.com>
2021-01-19 22:14:46 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -318,20 +318,6 @@ function ShowToilet(){
 }}
 
 
-/*---------- Google FormsのURLにデータを付加 ----------*/
-function PlusURL(){
-  alert("ご利用ありがとうございました！"+"\n"+"最後に数分だけ、アンケートへご協力をお願いします。");
-  var plusData = "「送信」ボタンを押してください。　　　　　";
-  var data = localStorage.getItem('Data');
-  if(data == null){
-  }else{
-    plusData += data;
-  }
-  alert("plusData： "+plusData);
-  return plusData;
-}
-
-
 /*---------- ハンバーガーメニュー ----------*/
 $(function(){
   $('.hamburger').click(function(){
